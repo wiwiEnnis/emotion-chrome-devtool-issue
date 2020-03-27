34d5b23f052d6e240f8f56544d194ea3c7597705
wiwiEnnis <ennis.lu@pentium.network>
2020-03-27 18:22:01 +0800

    setup issue env

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React from 'react';
+import { css } from '@emotion/core';
 
-const App = ({ title }) =>
-  <div>{title}</div>;
+const AppStyle = css`
+  font-size: 2em;
+
+  
+`;
+
+const AppChildStyle = css`
+  font-style: italic;
+
+  p {
+    font-weight: bold;
+  }
+`;
+
+const App = () => (
+  <div css={AppStyle}>
+    <div css={AppChildStyle}>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora iusto vero officia facilis
+        modi qui odio consequatur quaerat ut dolorum itaque fugit reiciendis, voluptatum quasi
+        necessitatibus dicta accusantium aspernatur quisquam.
+      </p>
+    </div>
+  </div>
+);
 
 export default App;
